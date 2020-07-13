0b308f7dcedfd6add673a2e21a34df63d13389a8
Jason He <jason.he@instacart.com>
2020-07-13 01:48:20 -0700

    add comment

--- app/channels/comments_channel.rb
+++ app/channels/comments_channel.rb
@@ -8,6 +8,7 @@ class CommentsChannel < ApplicationCable::Channel
     stop_all_streams
   end
 
+  # This action method is an example of rails flow triggered by client-to-server data over web socket (via AnyCable-Twirp gem)
   def comment(data)
     message = Message.find(data["message_id"].to_i)
     user = User.find(data["user_id"].to_i)
