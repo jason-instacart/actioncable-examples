54cdcaf929fb544c73f41313d7d1e8ce38754b39
Jason He <jason.he@instacart.com>
2020-07-13 01:29:31 -0700

    implement comment action

--- app/channels/comments_channel.rb
+++ app/channels/comments_channel.rb
@@ -7,4 +7,10 @@ class CommentsChannel < ApplicationCable::Channel
   def unfollow
     stop_all_streams
   end
+
+  def comment(data)
+    message = Message.find(data["message_id"].to_i)
+    user = User.find(data["user_id"].to_i)
+    Comment.create!(content: data["content"], message: message, user: user)
+  end
 end
